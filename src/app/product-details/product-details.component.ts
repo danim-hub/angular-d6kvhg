7ed92ab5436995b567a8765cea80f1a7f16c49f0
danim-hub <danimx@gmail.com>
2021-05-13 21:49:02 -0500

    3 files modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,12 +11,18 @@ import { products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product;
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) {}
 
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
     this.product = products.find(product => product.id === productIdFromRoute)
+  }
+
+  addToCart(product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 }
